22a199ee6b7b446e7b73d9bef463bce48ac46ae4
BossHuang <myheromyjoe@163.com>
2017-04-09 15:46:59 +0800

    spider

--- testEfficiencyOfRequests.py
+++ testEfficiencyOfRequests.py
@@ -1,27 +1,44 @@
 __author__ = 'leihuang'
 import requests
 import time
-import matplotlib.pyplot as plt
+import sys
 
 # test the efficiency of requests
 
+# 1.
+# requests the same page
+# with the request count increaseing, how the time will take
 url = "https://www.baidu.com"
 requestCount = []
 requestTime = []
 avgRequestTime = []
 start = time.time()
-for i in xrange(1,10001):
+for count in xrange(1,10001):
     requests.get(url)
-    if i%100 == 0:
+    if count%100 == 0:
         now = time.time()
-        requestCount.append(i)
+        requestCount.append(count)
         requestTime.append(now-start)
         avgRequestTime.append(requestTime[-1]*1.0/requestCount[-1])
 
-plt.scatter(requestCount,requestTime)
-plt.show()
 print requestCount
 print requestTime
 print avgRequestTime
-plt.scatter(requestCount,avgRequestTime)
-plt.show()
+
+# 2.
+# with requesting the different pages, how the time will take
+urlSmall = "https://www.baidu.com"
+urlMidium = "http://xiaodaoma.baijia.baidu.com/article/819314"
+urlBig = "http://news.baidu.com"
+urls = [urlSmall,urlMidium,urlBig]
+# the size of page
+for index in range(3):
+    r = requests.get(urls[index])
+    if r.status_code == 200:
+        print "the size of %s in bytes is %s" %(urls[index], sys.getsizeof(r.text))
+# the time spend
+for index in range(3):
+    begin = time.time()
+    for count in range(100):
+        requests.get(urls[index])
+    print "%s spend %s" %(urls[index], time.time()-begin)
