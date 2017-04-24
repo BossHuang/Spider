9cbcecfd5d4dad748d3ee7206f996e65e2f0f5a8
BossHuang <myheromyjoe@163.com>
2017-04-24 22:06:03 +0800

    repair

--- testEfficiencyOfRequests.py
+++ testEfficiencyOfRequests.py
@@ -2,6 +2,22 @@ __author__ = 'leihuang'
 import requests
 import time
 import sys
+import matplotlib.pyplot as plt
+
+def calAvgAndSTD(numberList):
+    '''
+    calculate the average and standard devation of number list
+    :param numberList:
+    :return average, standard deviation
+    '''
+    # std = sqrt(Dx) = E(x^2)-(Ex)^2
+    avg = 0.0
+    size = len(numberList)
+    squareAvg = 0.0
+    for item in numberList:
+        avg += item*1.0/size
+        squareAvg += item*1.0/size*item
+    return avg, squareAvg - avg*avg
 
 # test the efficiency of requests
 
@@ -11,19 +27,21 @@ import sys
 url = "https://www.baidu.com"
 requestCount = []
 requestTime = []
-avgRequestTime = []
-start = time.time()
 for count in xrange(1,10001):
+    start = time.time()
     requests.get(url)
-    if count%100 == 0:
-        now = time.time()
-        requestCount.append(count)
-        requestTime.append(now-start)
-        avgRequestTime.append(requestTime[-1]*1.0/requestCount[-1])
+    now = time.time()
+    requestCount.append(count)
+    requestTime.append(now-start)
 
-print requestCount
-print requestTime
-print avgRequestTime
+avg,std = calAvgAndSTD(requestTime)
+print "avg:%s, std:%s" %(avg, std)
+
+plt.title("request count and time ")
+plt.xlabel("count")
+plt.ylabel("time/s")
+plt.scatter(requestCount,requestTime)
+plt.show()
 
 # 2.
 # with requesting the different pages, how the time will take
@@ -32,13 +50,31 @@ urlMidium = "http://xiaodaoma.baijia.baidu.com/article/819314"
 urlBig = "http://news.baidu.com"
 urls = [urlSmall,urlMidium,urlBig]
 # the size of page
+urlSize = []
 for index in range(3):
     r = requests.get(urls[index])
     if r.status_code == 200:
-        print "the size of %s in bytes is %s" %(urls[index], sys.getsizeof(r.text))
+        size = sys.getsizeof(r.text)
+        print "the size of %s in bytes is %s" %(urls[index], size)
+        urlSize.append(size)
 # the time spend
+timeSize = []
 for index in range(3):
     begin = time.time()
     for count in range(100):
         requests.get(urls[index])
-    print "%s spend %s" %(urls[index], time.time()-begin)
+    now = time.time()
+    timeSize.append((now-begin)/100)
+    print "%s spend %s" %(urls[index], now-begin)
+
+plt.title("the size of page and request time")
+plt.xlabel("size")
+plt.ylabel("average time")
+plt.scatter(urlSize,timeSize)
+plt.show()
+
+
+
+
+
+
